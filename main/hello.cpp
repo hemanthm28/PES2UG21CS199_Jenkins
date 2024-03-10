9101f35a17788302c5d1bebe903d08461a516f12
hemanthm28 <104125619+hemanthm28@users.noreply.github.com>
2024-03-10 23:40:47 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins by PES2UG21CS199 \n";
+  cout << "successfully built and run" ;
   cout << "I have successfully built and run\n";
   return 0;
 }
